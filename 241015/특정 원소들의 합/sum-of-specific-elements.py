5ea5666cb48b67f41b5a628f37c9df68601fb2ef
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-15 10:18:58 +0900

    Title: 특정 원소들의 합, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/특정 원소들의 합/sum-of-specific-elements.py
+++ 241015/특정 원소들의 합/sum-of-specific-elements.py
@@ -1,12 +1,25 @@
+# arr_2d = [
+#     list(map(int, input().split()))
+#     for _ in range(4)
+# ]
+
+
+
+# sum_val = 0
+# for i in range(4):
+# 	for j in range(i+1):
+# 		sum_val += arr_2d[i][j]
+# print(sum_val)
+
+
+## enumerate
 arr_2d = [
     list(map(int, input().split()))
     for _ in range(4)
 ]
 
+totalsum= 0
 
-
-sum_val = 0
-for i in range(4):
-	for j in range(i+1):
-		sum_val += arr_2d[i][j]
-print(sum_val)
+for j, i in enumerate(arr_2d, start=1):
+    totalsum += sum(i[ :j])
+print(totalsum)
