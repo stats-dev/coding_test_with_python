59d0dd78abbba43afdb4d6e02b880ce3328da6aa
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-21 11:50:07 +0900

    Title: 함수를 이용한 윤년 판별, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241021/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,12 @@
+def is_yunyear(n):
+    ## 4로 나누어지면 윤년
+    if n % 4 == 0:
+        return "true"
+    elif n % 100 == 0 and n % 400 != 0:
+        return "true"
+    return "false"
+
+y = int(input())
+
+yun = is_yunyear(y)
+print(yun)
