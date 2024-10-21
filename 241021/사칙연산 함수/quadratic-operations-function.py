bb68fb59924c0c6f70257b322671ed0d5b6f496a
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-21 16:58:46 +0900

    Title: 사칙연산 함수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 241021/사칙연산 함수/quadratic-operations-function.py
+++ 241021/사칙연산 함수/quadratic-operations-function.py
@@ -3,11 +3,15 @@ result = 0
 
 if o == '+':
     result = int(a) + int(c)
+    print(f"{a} {o} {c} = {result}")
 elif o == '-':
     result = int(a) - int(c)
+    print(f"{a} {o} {c} = {result}")
 elif o == '/':
-    result = int(a) / int(c)
-else:
+    result = int(a) // int(c)
+    print(f"{a} {o} {c} = {result}")
+elif o == '*':
     result = int(a) * int(c)
-
-print(f"{a} {o} {c} = {result}")
+    print(f"{a} {o} {c} = {result}")
+else:
+    print(False)
