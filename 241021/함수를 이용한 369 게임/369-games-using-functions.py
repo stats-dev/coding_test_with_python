8b4c2264d11c25dcaad0a2325265e9f4b9cd963c
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-21 10:38:05 +0900

    Title: 함수를 이용한 369 게임, Time: 154ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241021/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,35 @@
+## 3의 배수 아니다.
+## 각 자리 숫자가 다른 경우만
+
+# def all_diff(n):
+#     return (n // 10) != (n % 10)
+
+# def is_ma_num(n):
+#     return n % 3 !=0 and all_diff(n) ## return 값이 true
+
+# cnt = 0
+# for i in range(10, 100): ## 두자리 수
+#     if is_ma_num(i):
+#         cnt += 1
+
+# print(cnt)
+
+a, b = tuple(map(int, input().split()))
+
+## a <= x <= b 3,6,9 하나 or 숫자가 3배수
+
+
+def is_threes(n):
+    threes = [3, 6, 9]
+    return (n // 10) in threes or (n % 10) in threes or is_sam(n)
+
+def is_sam(n):
+    return (n % 3 == 0)
+
+cnt = 0
+
+for i in range(a, b+1):
+    if is_threes(i):
+        cnt += 1
+
+print(cnt)
