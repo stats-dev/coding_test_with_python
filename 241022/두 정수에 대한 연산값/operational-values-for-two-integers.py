dbc10468a2dcc65402e16560939ad3c50b4510e2
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 15:59:37 +0900

    Title: 두 정수에 대한 연산값, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 241022/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,9 +1,13 @@
 a, b = map(int, input().split())
 
 def min_args(a, b):
-    n1 = min(a, b) * 2
-    n2 = max(a, b) + 25
-    return n1, n2
+    if a > b:
+        a = a + 25
+        b = b * 2
+    else:
+        b = b + 25
+        a = a * 2
+    return a, b
 
 
 result = min_args(a,b)
