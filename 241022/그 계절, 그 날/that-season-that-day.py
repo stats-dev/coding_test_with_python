a582ca28c99f2baf73b33af3dd79d8f7329e05dc
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 13:42:38 +0900

    Title: 그 계절, 그 날, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/그 계절, 그 날/that-season-that-day.py
+++ 241022/그 계절, 그 날/that-season-that-day.py
@@ -6,14 +6,16 @@ def is_yun(year):
     return False
 
 def is_season(month):
-    if month > 2:
+    if month in [3,4,5]:
         print("Spring")
-    elif month > 5:
+    elif month in [6,7,8]:
         print("Summer")
-    elif month > 8:
+    elif month in [9,10,11]:
         print("Fall")
-    else:
+    elif month in [12,1,2]:
         print("Winter")
+    else:
+        print(-1)
 
 def is_last_day(year, month):
     if is_yun(year):
