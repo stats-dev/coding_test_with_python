3e01ccdb788ec98e23ee8297af04ea802212eda1
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 13:40:09 +0900

    Title: 그 계절, 그 날, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241022/그 계절, 그 날/that-season-that-day.py
+++ 241022/그 계절, 그 날/that-season-that-day.py
@@ -1,11 +1,7 @@
 Y, M, D = tuple(map(int, input().split()))
 
 def is_yun(year):
-    if year % 4 == 0:
-        return True
-    elif year % 100 == 0:
-        return False
-    elif year % 400 == 0:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     return False
 
