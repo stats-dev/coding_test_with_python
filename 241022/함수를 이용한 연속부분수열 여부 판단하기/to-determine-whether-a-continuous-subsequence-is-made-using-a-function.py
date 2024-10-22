5f4af1ae9fb259308973b9a7a7d2cdb223d1e69a
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 11:24:47 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 241022/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -3,10 +3,29 @@ n1, n2 = tuple(map(int, input().split()))
 A = list(tuple(map(int, input().split())))
 B = list(tuple(map(int, input().split())))
 
-def is_continuous_subsequence(sub, main):
-    return any(main[i:i + len(sub)] == sub for i in range(len(main) - len(sub) + 1))
+# def is_continuous_subsequence(sub, main):
+#     return any(main[i:i + len(sub)] == sub for i in range(len(main) - len(sub) + 1))
 
-if is_continuous_subsequence(B, A):
+# if is_continuous_subsequence(B, A):
+#     print("Yes")
+# else:
+#     print("No")
+
+## n to n2 까지 인덱스 값에서 동일 배열 확인
+def is_same(n):
+    for i in range(n2):
+        if A[i+n] != B[i]:
+            return False ## 하나라도 틀리면 안됨.
+    return True
+
+
+def is_seq():
+    for i in range(n1 - n2 + 1):
+        if is_same(i):
+            return True
+    return False
+
+if is_seq():
     print("Yes")
 else:
     print("No")
