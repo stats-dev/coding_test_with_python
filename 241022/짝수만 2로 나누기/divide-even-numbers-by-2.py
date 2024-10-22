ebe1caa2cb3c0beb232e9ca3b0b0625856c8c3ef
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 14:55:08 +0900

    Title: 짝수만 2로 나누기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 241022/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -1,16 +1,31 @@
-def even_only_div(lst):
-    result = []
-    for i in lst:
-        if i % 2 == 0:
-            result.append(i // 2)
-        else:
-            result.append(i)
-    return result
+# def even_only_div(lst):
+#     result = []
+#     for i in lst:
+#         if i % 2 == 0:
+#             result.append(i // 2)
+#         else:
+#             result.append(i)
+#     return result
 
-N = int(input())
-ns = map(int, input().split())
+# N = int(input())
+# ns = map(int, input().split())
 
-ans = even_only_div(ns)
+# ans = even_only_div(ns)
 
-for i in ans:
-    print(i, end = " ")
+# for i in ans:
+#     print(i, end = " ")
+
+# 변수 선언 및 입력:
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+def modify(arr):
+    for i in range(n):
+        if arr[i] % 2 == 0:
+            arr[i] //= 2
+
+
+modify(arr)
+for elem in arr:
+    print(elem, end=" ")
