46219ad5bdea745f201eff09b3ddb92579acff61
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 12:54:40 +0900

    Title: 2021년 날짜의 유무, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/2021년 날짜의 유무/with-or-without-2021.py
+++ 241022/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,10 +1,11 @@
 M, D = map(int, input().split())
 
 def is_month(m, d):
+    throne = [1,3,5,7,8,10,12]
     while m > 0 and m < 13:
         if m == 2:
             return d < 29
-        elif m % 2 == 0:
+        elif m in throne:
             return d < 32
         else:
             return d < 31
