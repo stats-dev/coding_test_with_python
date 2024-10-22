8e875e48349c951524550202f11ed2fa1a0e4e4b
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 16:36:49 +0900

    Title: 절댓값으로 변경, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/절댓값으로 변경/find-the-absolute-value.py
+++ 241022/절댓값으로 변경/find-the-absolute-value.py
@@ -1,11 +1,27 @@
-N = int(input())
+# N = int(input())
 
-arr = tuple(map(int, input().split()))
+# arr = tuple(map(int, input().split()))
 
-def is_abs(a):
-    if a < 0:
-        a = -a
-    print(a, end=" ")
+# def is_abs(a):
+#     if a < 0:
+#         a = -a
+#     print(a, end=" ")
 
-for a in arr:
-    is_abs(a)
+# for a in arr:
+#     is_abs(a)
+
+## 리스트는 mmutable 하다.
+n = int(input())
+arr = list(map(int, input().split()))
+
+def abs_val(arr):
+    for i in range(n):
+        if arr[i] < 0:
+            arr[i] = -arr[i]
+    
+    return
+
+abs_val(arr)
+
+for e in arr:
+    print(e, end=" ")
