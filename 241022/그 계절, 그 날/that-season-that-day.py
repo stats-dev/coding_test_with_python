7e19c336bd9801d01da740faa4572ea4e9b0c458
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-22 13:24:35 +0900

    Title: 그 계절, 그 날, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241022/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,38 @@
+Y, M, D = tuple(map(int, input().split()))
+
+def is_yun(year):
+    if year % 4 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 400 == 0:
+        return True
+    return False
+
+def is_season(month):
+    if month > 2:
+        print("Spring")
+    elif month > 5:
+        print("Summer")
+    elif month > 8:
+        print("Fall")
+    else:
+        print("Winter")
+
+def is_last_day(year, month):
+    if is_yun(year):
+        if month == 2:
+            return 29
+    else:
+        if month == 2:
+            return 28
+    
+    if month in [4,6,9,11]:
+        return 30
+    else:
+        return 31
+
+if D <= is_last_day(Y, M):
+    is_season(M)
+else:
+    print(-1)
