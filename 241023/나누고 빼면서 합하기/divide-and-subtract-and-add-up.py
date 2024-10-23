aea5b213350f3d7d0f958830f718885e5e1aa111
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-23 13:23:10 +0900

    Title: 나누고 빼면서 합하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 241023/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 241023/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -1,18 +1,16 @@
 n, m = tuple(map(int, input().split()))
 
-A = list(map(int, input().split()))
+A = [0] + list(map(int, input().split()))
 sum = 0
 
 def sum_vals(m):
     global sum
-    while m > 1:
+    while m > 0:
         sum += A[m]
         if m % 2 == 0:
             m //= 2
-            sum += A[m]
         else:
             m -= 1
-            sum += A[m]
     return sum
 
 print(sum_vals(m))
