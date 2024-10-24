719f3a7211882d4673a4fd682d20646fbf175b40
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-24 15:49:55 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 241024/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 241024/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -1,15 +1,31 @@
-N = int(input())
+# N = int(input())
 
 
-def sum_even_odd(n):
-    if n == 0:
-        return n
-    if n == 1:
-        return 1
+# def sum_even_odd(n):
+#     if n == 0:
+#         return n
+#     if n == 1:
+#         return 1
     
-    if n % 2 != 0:
-        return sum_even_odd(n - 2) + n
-    else:
-        return sum_even_odd(n - 2) + n
+#     if n % 2 != 0:
+#         return sum_even_odd(n - 2) + n
+#     else:
+#         return sum_even_odd(n - 2) + n
 
-print(sum_even_odd(N))
+# print(sum_even_odd(N))
+
+## 홀 짝이 같은 수의 합. 홀은 홀만, 짝은 짝만.
+
+n = int(input())
+
+
+# 1부터 n까지의 n과 홀짝이 같은 수들의 합을 반환합니다.
+def get_num(n):
+    if n <= 2:
+        return n
+
+    # n과 홀짝이 같은 수만을 재귀함수로 호출합니다.
+    return get_num(n - 2) + n
+
+
+print(get_num(n))
