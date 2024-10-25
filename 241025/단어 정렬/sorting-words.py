9fb0bb06ad3aed6dc0be56b989d1d3734330829e
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-25 16:55:39 +0900

    Title: 단어 정렬, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 241025/단어 정렬/sorting-words.py
+++ 241025/단어 정렬/sorting-words.py
@@ -1,9 +1,19 @@
+# n = int(input())
+
+# strs = [input() for _ in range(n)]
+
+
+# strs.sort()
+
+# for e in strs:
+#     print(e)
+
+import sys
+
 n = int(input())
-
-strs = [input() for _ in range(n)]
-
-
+strs = sys.stdin.readlines()
+# strs = [str.strip() for str in strs]
+strs = [s.strip() for s in strs]
 strs.sort()
 
-for e in strs:
-    print(e)
+for e in strs: print(e)
