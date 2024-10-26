5446ea40a59dafe69578e4a90c3dcd01cbe09dde
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-26 14:58:00 +0900

    Title: 코드네임, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241026/코드네임/code-name.py
@@ -0,0 +1,17 @@
+## 객체를 원소로
+
+class Codename:
+    def __init__(self, codename='', score=0):
+        self.codename = codename
+        self.score = score
+
+
+codenames = [Codename(*tuple(input().split())) for _ in range(5)]
+
+min_cn = Codename()
+
+for i in range(5):
+    if int(codenames[i].score) <= int(codenames[0].score):
+        min_cn = codenames[i]
+
+print(min_cn.codename, min_cn.score, end= " ")
