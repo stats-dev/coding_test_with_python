90b57c7b7463437395d2d73f243d683c4fe5b939
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-26 15:09:20 +0900

    Title: 코드네임, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 241026/코드네임/code-name.py
+++ 241026/코드네임/code-name.py
@@ -8,12 +8,17 @@ class Codename:
 
 codenames = [Codename(*tuple(input().split())) for _ in range(5)]
 
-min_cn = Codename()
+# min_cn = Codename()
 
-if codenames:
-    min_cn = codenames[0]
-    for code in codenames:
-        if int(code.score) < int(min_cn.score):
-            min_cn = code
+# if codenames:
+#     min_cn = codenames[0]
+#     for code in codenames:
+#         if int(code.score) < int(min_cn.score):
+#             min_cn = code
+
+##혹은 lambda x : min(int(x.score)) 를 활용할 수 있음.
+
+min_cn = min(codenames, key = lambda x : int(x.score))
+
 
 print(min_cn.codename, min_cn.score, end= " ")
