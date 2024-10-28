ac810d987f53933b1a6cf89605c021c9cbf183e0
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-28 14:04:27 +0900

    Title: 줄 세우기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241028/줄 세우기/line-up-students.py
@@ -0,0 +1,35 @@
+## class
+
+class Student:
+    def __init__(self, height, weight, rank):
+        self.height = height
+        self.weight = weight
+        self.rank = None
+
+# for rank, num in enumerate(numx, start=1):
+#     num_to_rank[num] = rank
+
+## 키 내림차순, 몸무게, 번호 내림차순
+# 번호를 먼저 지정함. rank, enumerate 활용
+n = int(input())
+
+students = [
+    list(tuple(map(int, input().split())))
+    for _ in range(n)
+]
+
+
+arr = []
+
+for student in students:
+    height, weight = student
+    arr.append(Student(height, weight, 0))
+
+
+for idx, s in enumerate(arr, start=1):
+    s.rank = idx
+
+arr.sort(key = lambda x : (-x.height, -x.weight, -x.rank))
+
+for s in arr:
+    print(s.height, s.weight, s.rank)
