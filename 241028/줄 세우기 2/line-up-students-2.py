6234933533b03d87b7571e12d4491e04fb278211
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-28 20:35:15 +0900

    Title: 줄 세우기 2, Time: 190ms, Memory: 25MB, Status: Passed - CodeTree

--- 241028/줄 세우기 2/line-up-students-2.py
+++ 241028/줄 세우기 2/line-up-students-2.py
@@ -5,12 +5,18 @@ students = [
     for _ in range(n)
 ]
 
-students.sort(key = lambda x: (x[0], -x[1]))
-
 arr = [(s, idx) for s, idx in enumerate(students, start=1)]
-# for student, idx in enumerate(students, start=1):
-#     students.append(student)
+
+
+arr.sort(key = lambda x: (x[1][0], -x[1][1]))
 
 for i in range(n):
     idx, (h, w) = arr[i]
     print(h, w, idx)
+
+# for student, idx in enumerate(students, start=1):
+#     students.append(student)
+
+# for i in range(n):
+#     idx, (h, w) = arr[i]
+#     print(h, w, idx)
