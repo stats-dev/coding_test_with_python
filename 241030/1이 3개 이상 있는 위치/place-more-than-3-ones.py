b69b4d6feeed16574308377ac5d5c25693ef2636
Stats <35090655+stats-dev@users.noreply.github.com>
2024-10-30 17:52:15 +0900

    Title: 1이 3개 이상 있는 위치, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241030/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 241030/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -21,7 +21,6 @@ arr = [
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
 xys = [(x, y) for x, row in enumerate(arr) for y, val in enumerate(row) if val == 1]
-# print(xys)
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
@@ -33,7 +32,7 @@ for x, y in xys:
         nx, ny = x + dx, y + dy
         if in_range(nx, ny) and arr[nx][ny] == 1:
             cnt += 1
-    if cnt > 4:
+    if cnt >= 3:
         total += 1
 
 print(total)
