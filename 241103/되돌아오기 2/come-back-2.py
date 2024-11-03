47fab09ae94dd6f1afb93d88abce0d0c20f2b696
Stats <35090655+stats-dev@users.noreply.github.com>
2024-11-03 18:59:20 +0900

    Title: 되돌아오기 2, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 241103/되돌아오기 2/come-back-2.py
+++ 241103/되돌아오기 2/come-back-2.py
@@ -1,3 +1,4 @@
+'''
 ## 시작
 x, y = 0, 0
 
@@ -29,3 +30,42 @@ for d in direcs:
         break
 
 print(answer)
+'''
+
+dirs = input() ## 변수 선언 입력
+x, y = 0, 0
+currdir = 3 ## 3번이 왜 시작? 북쪽이라서?
+
+## 동남서북 순 정의
+dxs = [1, 0, -1, 0]
+dys = [0, -1, 0, 1]
+
+# flag는 시작점으로 되돌아 왔는지 봅니다.
+flag = False
+
+leng = len(dirs)
+
+## move
+for i in range(leng):
+    cdir = dirs[i]
+    # re 90 rotate
+    if cdir == 'L':
+        currdir = (currdir - 1 + 4) % 4
+
+    ## 90 +1 rotate
+    elif cdir == 'R':
+        currdir = (currdir + 1) % 4
+    
+    ## forward
+    else:
+        x, y = x + dxs[currdir], y + dys[currdir]
+    
+    # 00
+    if x == 0 and y == 0:
+        print(i + 1)
+        flag = True
+        break ## break 반드시 필요
+    
+## no return
+if flag == False:
+    print(-1)
