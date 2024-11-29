47a777b47dfdad15cb1d35c78024f2394dcce4a8
Stats <35090655+stats-dev@users.noreply.github.com>
2024-11-29 16:53:40 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

--- 241129/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 241129/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -1,11 +1,23 @@
-blank = list(input()) ## 빈칸 필요한데,
+# blank = list(input()) ## 빈칸 필요한데,
 
-answer = 0
+# answer = 0
 
-for i in range(len(blank)):
-    for j in blank[i:]:
-        if blank[i]+j=='()':
-            answer += 1
+# for i in range(len(blank)):
+#     for j in blank[i:]:
+#         if blank[i]+j=='()':
+#             answer += 1
 
-print(answer)
+# print(answer)
 
+## 간단 해결법, 일일이 잡아보면, 순서대로 문자 확인 ()
+
+string = input()
+n = len(string)
+
+cnt = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        if string[i] == '(' and string[j] == ')':
+            cnt += 1
+
+print(cnt)
