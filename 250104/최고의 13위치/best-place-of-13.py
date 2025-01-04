5f4649f0dce7023ca684bb5ea03ac92c0c43e8b1
Stats <35090655+stats-dev@users.noreply.github.com>
2025-01-04 18:02:05 +0900

    Title: 최고의 13위치, Time: 56ms, Memory: 17MB, Status: Passed - CodeTree

--- 250104/최고의 13위치/best-place-of-13.py
+++ 250104/최고의 13위치/best-place-of-13.py
@@ -7,7 +7,7 @@ max_cnt = 0
 for i in range(n):
     for j in range(n - 1):
         for k in range(n - 2):
-            max_cnt = max(max_cnt, grid[i][j] + grid[i][j + 1] + grid[i][k + 2])
+            max_cnt = max(max_cnt, grid[i][j] + grid[i][j + 1] + grid[j][k + 2])
 
 print(max_cnt)
 
