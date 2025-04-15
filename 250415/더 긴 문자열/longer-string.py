52fa85f22414d603dba6dfb20fc7ce488cf4c457
Stats <35090655+stats-dev@users.noreply.github.com>
2025-04-15 12:37:19 +0900

    Title: 더 긴 문자열, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250415/더 긴 문자열/longer-string.py
@@ -0,0 +1,8 @@
+sentences = list(input().split())
+
+if len(sentences[0]) == len(sentences[1]):
+    print('same')
+elif len(sentences[0]) > len(sentences[1]):
+    print('Coding' + str(len(sentences[0])))
+else:
+    print('Coding ' + str(len(sentences[1])))
