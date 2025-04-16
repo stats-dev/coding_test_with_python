c52d7217dbadf0e7e0cccc46fa3a6ecba8507ff2
Stats <35090655+stats-dev@users.noreply.github.com>
2025-04-17 08:30:30 +0900

    Title: 두 개의 격자 비교하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250417/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+
+mat1 = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+mat2 = [
+    list(map(int, input().split())) 
+    for _ in range(n)
+    ]
+
+newmat = [
+    [1 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(n):
+    for j in range(n):
+        if mat1[i][j] == mat2[i][j]:
+            newmat[i][j] = 0
+        print(newmat[i][j], end=" ")
+    print()
+
+
