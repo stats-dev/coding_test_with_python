adbe4cb04251bc54f57d5188ae32877a392a6ad1
Stats <35090655+stats-dev@users.noreply.github.com>
2025-04-18 08:00:56 +0900

    Title: 달에서 무게 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250418/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,9 @@
+# print("%.4f" % 33.12345)
+# print("{0:.4f}".format(33.12345))
+# print(f"{33:.4f}")
+
+## 변수 저장
+weight, rate = 13, 0.165
+
+## 무게 곱하기 중력 비율 식과 출력
+print(f"{weight} * {rate:.6f} = {weight * rate:.6f}")
