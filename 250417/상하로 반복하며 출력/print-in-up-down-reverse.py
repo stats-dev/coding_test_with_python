c67752f89db92f653dca3875b843fcaaba7298ba
Stats <35090655+stats-dev@users.noreply.github.com>
2025-04-17 11:39:21 +0900

    Title: 상하로 반복하며 출력, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250417/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ 250417/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -1,20 +1,31 @@
+# n = int(input())
+
+
+# arr = [
+#     [0 for _ in range(n)]
+#     for _ in range(n)
+# ]
+
+
+# for i in range(n):
+#     for j in range(n):
+#         if i % 2 != 0:
+#             arr[j][i] = n - j
+#         else:
+#             arr[j][i] = j + 1
+        
+# for i in range(n):
+#     for j in range(n):
+#         print(arr[i][j], end="")
+#     print()
+
+
 n = int(input())
 
-
-arr = [
-    [0 for _ in range(n)]
-    for _ in range(n)
-]
-
-
 for i in range(n):
     for j in range(n):
-        if i % 2 != 0:
-            arr[j][i] = n - j
+        if j % 2 == 0: ## 짝수라면,
+            print(i + 1, end="")
         else:
-            arr[j][i] = j + 1
-        
-for i in range(n):
-    for j in range(n):
-        print(arr[i][j], end="")
+            print(n - i, end="")
     print()
